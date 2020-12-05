c0355769cd4b0aad3293394015a37efa7947a0f1
margomto <mgomez@hpmgt.localdomain>
2020-12-05 18:57:38 +0100

    Modify dict c and save_ip

--- analyzer.py
+++ analyzer.py
@@ -1,9 +1,10 @@
 from collections import Counter
 
-def save_ip(ip_adress, ip_count):
+def save_ip(data):
     with open ('analysis.txt','w') as f:
-        data = ip_adress + ' ' + str(ip_count)
-        f.write(data)
+        for i in data:
+            register = i[0] + ' ' + str(i[1]) + '\n'
+            f.write(register)
         
 with open ('/var/log/nginx/access.log', 'r') as f:
     c = Counter()
@@ -13,10 +14,12 @@ with open ('/var/log/nginx/access.log', 'r') as f:
             break
         ip = log.split(' ')[0]
         ip_count = c.update({ip: 1})
-        c.most_common()
-        
-    for ip_address, ip_count in c.items():
-        save_ip(ip_address, ip_count)
+    sorted_c = {}
+    sorted_ip_c = sorted(c, key=c.get, reverse=True)
+    for addr in sorted_ip_c:
+        sorted_c[addr] = c[addr]
+    data = [(ip_address, ip_count) for ip_address, ip_count in sorted_c.items()]
+    save_ip(data)
 
 
         
